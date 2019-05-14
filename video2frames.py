70e2f9ebda7353519bfaf85bb02976ee54e7ec56
yevhen <yevhen.krasnokutsky@gmail.com>
2019-05-14 22:03:00 +0300

    tests of resolution and color inversion

--- video2frames.py
+++ video2frames.py
@@ -17,8 +17,8 @@ len_gscale67 = len(gscale67) - 1
 gscale10 = "@%#*+=-:. "
 len_gscale10 = len(gscale10) - 1
 
-WIDTH = 80
-HEIGHT = 24
+WIDTH = 320
+HEIGHT = 96
 SCALE = -1
 NEW_WIDTH, NEW_HEIGTH = 0, 0
 FRAME_NUMBER = 0
@@ -27,18 +27,19 @@ FRAME_NUMBER = 0
 # get scale, width, height, framerate, grayscale palette from args
 # implement drop frames to produce less ascii text from video
 
-path_to_save = os.path.join(os.getcwd(), 'result-ascii-gray')
+path_to_save = os.path.join(os.getcwd(), 'result-ascii-360x96-edges')
 path_to_video = os.path.join(os.getcwd(), 'test_720P_1500K_217812511.mp4')
 video = cv2.VideoCapture(path_to_video)
 
-if video.isOpened() and SCALE == -1:
+if video.isOpened():
     ret, frame = video.read()
     h, w, _ = frame.shape
     SCALE = WIDTH / w
     NEW_WIDTH = int(w * SCALE)
-    NEW_HEIGTH = int(h * SCALE)
+    NEW_HEIGTH = int(h * SCALE / 2)
     print('num of frames:', video.get(cv2.CAP_PROP_FRAME_COUNT))
     cv2.destroyAllWindows()
+
 
 while video.isOpened():
     ret, frame = video.read()
@@ -47,15 +48,15 @@ while video.isOpened():
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     h, w = gray.shape
     gray = cv2.resize(gray, (NEW_WIDTH, NEW_HEIGTH))
-    # edges = cv2.Canny(gray, 70, 120)
+    edges = ~cv2.Canny(gray, 75, 120)
     # laplacian = cv2.Laplacian(gray, ddepth=cv2.CV_16S, ksize=3)
-    # cv2.imshow('edges', edges)
+    cv2.imshow('edges', edges)
     ascii_frame = ""
     ascii_list = []
     ascii_list.append('\n')
     for row in range(NEW_HEIGTH):
         for col in range(NEW_WIDTH):
-            gsval = gscale67[int((gray[row, col]*len_gscale67)/255)]
+            gsval = gscale67[int((edges[row, col]*len_gscale67)/255)]
             ascii_list.append(gsval)
         ascii_list.append('\n')
     ascii_frame = ''.join(ascii_list)
